74ac5048401b080ffa6bc63d73da88bd81bacfd0
su-myn <134207439+su-myn@users.noreply.github.com>
2024-08-22 09:25:17 +0800

    implement 'complete' to streamlit (video 185)

--- web.py
+++ web.py
@@ -15,10 +15,18 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Labell", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
 
 
+print("Testing123")
+
+st.session_state
 
