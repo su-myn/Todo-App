4acb994f723bff909f6c2aabff5ef7954eca7062
su-myn <134207439+su-myn@users.noreply.github.com>
2024-08-22 08:57:47 +0800

    implement 'Add New Todo Items' to streamlit (video 184)

--- web.py
+++ web.py
@@ -6,6 +6,11 @@ print("Current working directory:", os.getcwd())
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
@@ -13,5 +18,7 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Labell", placeholder="Add new todo...")
+st.text_input(label="Labell", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+
+
 
